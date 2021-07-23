fed691863b1363e093cf82af8e44f32f7ca158ca
taneuzin <taneuzin@u.nus.edu>
2021-07-23 17:21:32 +0800

    organise files 2

--- src/App.js
+++ src/App.js
@@ -7,18 +7,15 @@ import {
 } from "react-router-dom";
 
 import Home from "./home/pages/Home";
-import Finance from "./finance/pages/Finance";
-// import Vbs from "./vbs/pages/Vbs";
+
+//Venue Booking System
 import VenueSelection from "./vbs/pages/VenueSelection/VenueSelection";
-import Cca from "./cca/pages/Cca";
-import Login from "./login/pages/Login";
-import Signup from "./signup/pages/Signup";
-import Navbar from "./shared/components/Navigation/NavBar";
-import Services from "./services/pages/Services";
-import Contacts from "./contacts/pages/Contacts";
 import SpecificVenue from "./vbs/pages/SpecificVenue/SpecificVenue";
 import BookingPage from "./vbs/pages/BookingPage/BookingPage";
 import ConfirmationPage from "./vbs/pages/ConfirmationPage/ConfirmationPage";
+
+import Navbar from "./shared/components/Navigation/NavBar";
+import Contacts from "./contacts/pages/Contacts";
 import { LoginContext } from "./shared/context/LoginContext";
 
 const App = () => {
@@ -47,47 +44,38 @@ const App = () => {
           path="/vbs/:venueName/bookingpage"
           component={BookingPage}
         />
-
-        <Route path="/finance" exact>
-          <Finance />
-        </Route>
-        <Route path="/cca" exact>
-          <Cca />
-        </Route>
-        <Route path="/Services" exact>
-          <Services />
-        </Route>
         <Router path="/Contacts" exact>
           <Contacts />
         </Router>
         <Redirect to="/" />
       </Switch>
     );
-  } else {
-    //routes when not logged in
-    routes = (
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <Route exact path="/vbs" component={VenueSelection}></Route>
-        <Route exact path="/vbs/confirmation" component={ConfirmationPage} />
-        <Route exact path="/vbs/:venueName" component={SpecificVenue} />
-        <Route
-          exact
-          path="/vbs/:venueName/bookingpage"
-          component={BookingPage}
-        />
-        <Route path="/login" exact>
-          <Login />
-        </Route>
-        <Route path="/signup" exact>
-          <Signup />
-        </Route>
-        <Redirect to="/" />
-      </Switch>
-    );
   }
+  // } else {
+  //   //routes when not logged in
+  //   routes = (
+  //     <Switch>
+  //       <Route exact path="/">
+  //         <Home />
+  //       </Route>
+  //       <Route exact path="/vbs" component={VenueSelection}></Route>
+  //       <Route exact path="/vbs/confirmation" component={ConfirmationPage} />
+  //       <Route exact path="/vbs/:venueName" component={SpecificVenue} />
+  //       <Route
+  //         exact
+  //         path="/vbs/:venueName/bookingpage"
+  //         component={BookingPage}
+  //       />
+  //       <Route path="/login" exact>
+  //         <Login />
+  //       </Route>
+  //       <Route path="/signup" exact>
+  //         <Signup />
+  //       </Route>
+  //       <Redirect to="/" />
+  //     </Switch>
+  //   );
+  // }
 
   return (
     <div>
--- src/cca/pages/Cca.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Cca = (props) => {
-    return ( <h2>page for cca</h2> );
-}
- 
-export default Cca;
