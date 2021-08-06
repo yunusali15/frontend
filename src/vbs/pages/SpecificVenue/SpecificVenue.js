73f3cd8db2a8e8fa987807467e4024e2b9d16c82
taneuzin <taneuzin@u.nus.edu>
2021-08-06 19:17:41 +0800

    implements timeslots refresh when date change

--- src/vbs/pages/SpecificVenue/SpecificVenue.js
+++ src/vbs/pages/SpecificVenue/SpecificVenue.js
@@ -9,40 +9,7 @@ import StatusBar from "../../shared/StatusBar";
 import "./SpecificVenue.css";
 import Modal from "react-modal";
 import axios from "axios";
-
-//data meant to be HTTP Requested from backend
-const TIMEDATA = [
-  { id: 0, timeStart: "0900 ", timeEnd: "0930", booked: false },
-  { id: 1, timeStart: "0930 ", timeEnd: "1000", booked: false },
-  { id: 2, timeStart: "1000 ", timeEnd: "1030", booked: false },
-  { id: 3, timeStart: "1030 ", timeEnd: "1100", booked: false },
-  { id: 4, timeStart: "1100 ", timeEnd: "1130", booked: false },
-  { id: 5, timeStart: "1130 ", timeEnd: "1200", booked: false },
-  { id: 6, timeStart: "1200 ", timeEnd: "1230", booked: false },
-  { id: 7, timeStart: "1230 ", timeEnd: "1300", booked: false },
-  { id: 8, timeStart: "1300 ", timeEnd: "1330", booked: false },
-  { id: 9, timeStart: "1330 ", timeEnd: "1400", booked: false },
-  { id: 10, timeStart: "1400", timeEnd: "1430", booked: false },
-  { id: 11, timeStart: "1430", timeEnd: "1500", booked: false },
-  { id: 12, timeStart: "1500", timeEnd: "1530", booked: false },
-  { id: 13, timeStart: "1530", timeEnd: "1600", booked: false },
-  { id: 14, timeStart: "1600", timeEnd: "1630", booked: false },
-  { id: 15, timeStart: "1630", timeEnd: "1700", booked: false },
-  { id: 16, timeStart: "1700", timeEnd: "1730", booked: false },
-  { id: 17, timeStart: "1730", timeEnd: "1800", booked: false },
-  { id: 18, timeStart: "1800", timeEnd: "1830", booked: false },
-  { id: 19, timeStart: "1830", timeEnd: "1900", booked: false },
-  { id: 20, timeStart: "1900", timeEnd: "1930", booked: false },
-  { id: 21, timeStart: "1930", timeEnd: "2000", booked: false },
-  { id: 22, timeStart: "2000", timeEnd: "2030", booked: false },
-  { id: 23, timeStart: "2030", timeEnd: "2100", booked: false },
-  { id: 24, timeStart: "2100", timeEnd: "2130", booked: false },
-  { id: 25, timeStart: "2130", timeEnd: "2200", booked: false },
-  { id: 26, timeStart: "2200", timeEnd: "2230", booked: false },
-  { id: 27, timeStart: "2230", timeEnd: "2300", booked: false },
-  { id: 28, timeStart: "2300", timeEnd: "2330", booked: false },
-  { id: 29, timeStart: "2330", timeEnd: "0000", booked: false },
-];
+import TIMEDATA from "./TIMEDATA";
 
 function SpecificVenue() {
   const [selectedDate, setSelectedDate] = useState(null);
@@ -70,13 +37,17 @@ function SpecificVenue() {
     console.log(bookedSlots);
     setSelectedDate(selectedDate);
     fetchAndSetTimeslots(selectedDate);
+    setSelectedTimeslot([]);
   }
 
   function fetchAndSetTimeslots(selectedDate) {
     const parsedDate = parseSelectedDate(selectedDate);
     const bookedArray = bookedSlots[`${parsedDate}`];
     console.log(bookedArray);
-    TIMEDATA.map((i) => (i.booked = false));
+    TIMEDATA.map((i) => {
+      i.booked = false;
+      i.selected = false;
+    });
     bookedArray && bookedArray.map((i) => (TIMEDATA[i].booked = true));
     setTimeslots(TIMEDATA);
   }
