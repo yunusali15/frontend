34cd7a4940bc4c8234464ffe268196adcc4f45af
taneuzin <taneuzin@u.nus.edu>
2021-08-08 01:19:18 +0800

    makes some changes to homepage implementation so it does not affect entire page

--- src/home/pages/Home.js
+++ src/home/pages/Home.js
@@ -3,7 +3,7 @@ import React, { useContext } from "react";
 import "./Home.css";
 import keviilogo from "../../shared/Images/keviilogo.png";
 import { LoginContext } from "../../shared/context/LoginContext";
-import homepage from './homepage.png';
+import homepage from "./homepage.png";
 
 const Home = (props) => {
   const auth = useContext(LoginContext);
@@ -12,14 +12,15 @@ const Home = (props) => {
       <div className="homeContainer">
         <div className="ctaContainer">
           <text className="title">
-          By KEVIIANS,
-          <br/>
-          &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-          For KEVIIANS
+            By KEVIIANS,
+            <br />
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; For KEVIIANS
           </text>
-          <a href='/vbs' className="cta">Try Our Venue Booking System</a>
+          <a href="/vbs" className="cta">
+            Try Our Venue Booking System
+          </a>
         </div>
-        <img src={homepage}/>
+        <img className="homepageImage" src={homepage} />
       </div>
       {/* {auth.isLoggedIn && <h3>you are now logged in</h3>} */}
     </React.Fragment>
