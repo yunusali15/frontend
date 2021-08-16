92ea67798db6d51888c3c57cf9e9050e1b827a23
taneuzin <taneuzin@u.nus.edu>
2021-08-17 00:35:50 +0800

    commit

--- src/vbs/pages/VenueSelection/VenueSelection.js
+++ src/vbs/pages/VenueSelection/VenueSelection.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { ReactRouter as Router, Link, useHistory } from "react-router-dom";
 import "./VenueSelection.css";
-import DATA from "./venueDATA";
 import StatusBar from "../../shared/StatusBar";
 import axios from "axios";
 import { BeatLoader } from "react-spinners";
@@ -16,38 +15,37 @@ const VenueSelection = () => {
   //fetch list of parent venues upon opening
   useEffect(() => {
     setLoading(true);
-    api
-      .get("/api/v1/venue/search?isChildVenue=false")
-      .then((res) => {
-        setParentVenueArray(res.data.venues);
-        setLoading(false);
-      });
+    api.get("/api/v1/venue/search?isChildVenue=false").then((res) => {
+      setParentVenueArray(res.data.venues);
+      setLoading(false);
+    });
     return;
   }, []);
 
   return (
     <div className="VenueSelection-page">
-      <StatusBar stage={1} /> 
-      {loading? <>
-            <p>Loading</p>
-            <BeatLoader color="black" loading={true} />
-          </>
-          :(
-      <div className="VenueSelection-venuesContainer">
-        {parentVenueArray.map((venue) => (
-          <Link
-            className="VenueSelection-venueLink"
-            to={{ pathname: `./vbs/${venue.id}`, state: { venue } }}
-          >
-            <div
-              className="VenueSelection-venue"
-              style={{ backgroundImage: `url(${BASEURL + venue.image})` }}
+      <StatusBar stage={1} />
+      {loading ? (
+        <>
+          <p>Loading</p>
+          <BeatLoader color="black" loading={true} />
+        </>
+      ) : (
+        <div className="VenueSelection-venuesContainer">
+          {parentVenueArray.map((venue) => (
+            <Link
+              className="VenueSelection-venueLink"
+              to={{ pathname: `./vbs/${venue.id}`, state: { venue } }}
             >
-              <div className="VenueSelection-venueName">{venue.name}</div>
-            </div>
-          </Link>
-        ))}
-      </div>
+              <div
+                className="VenueSelection-venue"
+                style={{ backgroundImage: `url(${BASEURL + venue.image})` }}
+              >
+                <div className="VenueSelection-venueName">{venue.name}</div>
+              </div>
+            </Link>
+          ))}
+        </div>
       )}
     </div>
   );
