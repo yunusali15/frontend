82d18b123f7ad40e9bec270d32fddfa5cd4468a3
Yunus <yunusali150201@gmail.com>
2021-08-29 00:00:11 +0800

    - Created Booking requests page with basic layout with react table to display the booking using mock data - Added component for Approve and reject links - Added Columns and checkboxes

--- src/admin/pages/AdminView.js
+++ src/admin/pages/AdminView.js
@@ -1,9 +1,11 @@
 import React from "react";
+import { BookingRequests } from "./BookingRequests/BookingRequests";
 
 const AdminView = () => {
   return (
     <div className="admin__view">
       <h1>This is the admin view page</h1>
+      {/* <BookingRequests /> */}
     </div>
   );
 };
