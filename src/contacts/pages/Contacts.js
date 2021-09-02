53eb969d55f7fbbd8ea045d0bf7aca2ed85aaf00
Yunus Ali <80503701+yunusali15@users.noreply.github.com>
2021-09-02 18:58:39 +0800

    Update Contacts.js

--- src/contacts/pages/Contacts.js
+++ src/contacts/pages/Contacts.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import BookingRequest from '../../admin/pages/BookingRequests/BookingRequest';
 import './Contacts.css';
 
 const Contacts = () => {
     return ( 
     <h1 className='ContactsPageMainDiv'>
         Please send your queries to ke7webdev@gmail.com 
-        <BookingRequest/>
     </h1>
     );
 }
