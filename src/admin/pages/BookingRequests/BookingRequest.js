71b6d58c312776a5144e4b9567d2963399d1e8a3
Tan Eu Zin <taneuzin@u.nus.edu>
2021-09-14 20:32:26 +0800

    set up filters

--- src/admin/pages/BookingRequests/BookingRequest.js
+++ src/admin/pages/BookingRequests/BookingRequest.js
@@ -1,25 +1,24 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import InnerTabs from "../InnerTabs";
 import Modal from "react-modal";
-import './BookingRequest.css'
+import "./BookingRequest.css";
 import PendingRequest from "./PendingRequest";
 import CompletedRequest from "./CompletedRequest";
-import FilterSort from "./FilterSort"
+import FilterSort from "./FilterSort";
 import axios from "axios";
 
 const BookingRequest = () => {
-  const [loading, setLoading] = useState(true); 
+  const [loading, setLoading] = useState(true);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [ccaFilter, setCcaFilter] = useState([]);
-  const [venueFilter ,setVenueFilter] = useState([]);
+  const [venueFilter, setVenueFilter] = useState([]);
   const [dateFilter, setDateFilter] = useState([]);
   const [sortBy, setSortBy] = useState([]);
-  const [pendingBookingRequest, setPendingBookingRequest] = useState([])
-  const [completedBookingRequest, setCompletedBookingRequest] = useState([])
-  const[fetchedCompletedRequest, setFetchedCompletedRequest] = useState(false);
+  const [pendingBookingRequest, setPendingBookingRequest] = useState([]);
+  const [completedBookingRequest, setCompletedBookingRequest] = useState([]);
+  const [fetchedCompletedRequest, setFetchedCompletedRequest] = useState(false);
   const [fetchedPendingRequest, setFetchedPendingRequest] = useState(false);
   const [isPending, setPending] = useState(true);
-
 
   const BASEURL = "https://britannic.herokuapp.com/";
 
@@ -27,13 +26,15 @@ const BookingRequest = () => {
   api.defaults.headers.common["Authorization"] = "KEVII1!";
 
   useEffect(() => {
-    fetchData()
+    fetchData();
   }, []);
 
   const fetchData = () => {
     api.get("/api/v1/bookingreq/all").then((res) => {
       console.log(res);
-      setPendingBookingRequest(res.data.bookingRequest.slice(0,res.data.bookingRequest.length));
+      setPendingBookingRequest(
+        res.data.bookingRequest.slice(0, res.data.bookingRequest.length)
+      );
       setFetchedPendingRequest(true);
     });
     api.get("/api/v1/bookingreq/all?APPROVED||REJECTED").then((res) => {
@@ -41,71 +42,90 @@ const BookingRequest = () => {
       setCompletedBookingRequest(res.data.bookingRequest);
       setFetchedCompletedRequest(true);
     });
-  }
+  };
 
   const modalStyle = {
     overlay: {
-        backgroundColor: "rgba(255, 255, 255, 0.75)",
+      backgroundColor: "rgba(255, 255, 255, 0.75)",
     },
     content: {
-        display: "flex",
-        flexDirection: "row",
-        top: '50%',
-        left: '50%',
-        right: 'auto',
-        bottom: 'auto',
-        marginRight: '-50%',
-        transform: 'translate(-50%, -50%)',
+      display: "flex",
+      flexDirection: "row",
+      top: "50%",
+      left: "50%",
+      right: "auto",
+      bottom: "auto",
+      marginRight: "-50%",
+      transform: "translate(-50%, -50%)",
     },
-}
+  };
 
   Modal.setAppElement("#root");
   return (
-    <div className='BookingRequestPageContainer'>
-    <InnerTabs>
-    <div style={{width: "100%"}} tabName="Pending" onClick = {() => setPending(true)}>
-    <div className='BookingRequestsPageSortBy' onClick={ ()=> setIsModalOpen(true)}>
-      Sort By
+    <div className="BookingRequestPageContainer">
+      <InnerTabs>
+        <div
+          style={{ width: "100%" }}
+          tabName="Pending"
+          onClick={() => setPending(true)}
+        >
+          <div
+            className="BookingRequestsPageSortBy"
+            onClick={() => setIsModalOpen(true)}
+          >
+            Sort By
+          </div>
+          {fetchedCompletedRequest && fetchedPendingRequest ? (
+            <PendingRequest
+              bookingRequest={pendingBookingRequest}
+              ccaFilter={ccaFilter}
+              dateFilter={dateFilter}
+              venueFilter={venueFilter}
+            />
+          ) : (
+            <div>Loading...</div>
+          )}
+        </div>
+        <div
+          tabName="Completed"
+          style={{ width: "100%" }}
+          onClick={() => setPending(false)}
+        >
+          <div
+            className="BookingRequestsPageSortBy"
+            onClick={() => setIsModalOpen(true)}
+          >
+            Sort By
+          </div>
+          {fetchedCompletedRequest && fetchedPendingRequest ? (
+            <CompletedRequest bookingRequest={completedBookingRequest} />
+          ) : (
+            <div>Loading...</div>
+          )}
+        </div>
+      </InnerTabs>
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={() => setIsModalOpen(false)}
+        style={modalStyle}
+      >
+        {
+          <FilterSort
+            setCcaFilter={setCcaFilter}
+            setDateFilter={setDateFilter}
+            setVenueFilter={setVenueFilter}
+            ccaFilter={ccaFilter}
+            dateFilter={dateFilter}
+            venueFilter={venueFilter}
+            setSortBy={setSortBy}
+            isPending={isPending}
+            setIsModalOpen={setIsModalOpen}
+            pendingBookingRequest={pendingBookingRequest}
+            completedBookingRequest={completedBookingRequest}
+          />
+        }
+      </Modal>
     </div>
-    {fetchedCompletedRequest && fetchedPendingRequest  ? (
-        <PendingRequest bookingRequest={pendingBookingRequest}/>
-    ): (
-    <div>Loading...</div>
-    )}
-    </div>
-    <div tabName="Completed" style={{width: "100%"}} onClick = {() => setPending(false)} >
-    <div className='BookingRequestsPageSortBy' onClick={ ()=> setIsModalOpen(true)}>
-      Sort By
-    </div>
-    {fetchedCompletedRequest && fetchedPendingRequest ? 
-    (
-      <CompletedRequest bookingRequest={completedBookingRequest}/>
-    ): 
-    (
-      <div>Loading...</div>
-    )}
-    </div>
-    </InnerTabs>
-    <Modal
-          isOpen={isModalOpen}
-          onRequestClose={() => setIsModalOpen(false)}
-          style={modalStyle}
-    >
-      {<FilterSort
-        setCcaFilter={setCcaFilter}
-        setDateFilter={setDateFilter}
-        setVenueFilter= {setVenueFilter}
-        ccaFilter={ccaFilter}
-        dateFilter={dateFilter}
-        venueFilter= {venueFilter}
-        setSortBy={setSortBy}
-        isPending = {isPending}
-        setIsModalOpen={setIsModalOpen}
-        pendingBookingRequest={pendingBookingRequest}
-        completedBookingRequest={completedBookingRequest}
-      /> }
-    </Modal>
-   </div>
   );
 };
 
--- src/admin/pages/BookingRequests/PendingRequest.js
+++ src/admin/pages/BookingRequests/PendingRequest.js
@@ -1,81 +1,108 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import ReactPaginate from "react-paginate";
-import {CheckBox} from './CheckBox.js'
+import { CheckBox } from "./CheckBox.js";
 import SpecificReqModal from "../SpecificReqModal/SpecificReqModal";
 
 const PendingRequest = (props) => {
-    const [modalIsOpen, setIsOpen] = useState(false);
-    const [bookingRequest, setbookingRequest] = useState(props.bookingRequest)
-    const [currentPage, setCurrentPage] = useState(0);
+  const PER_PAGE = 10;
+  const [currentPage, setCurrentPage] = useState(0);
+  const offset = currentPage * PER_PAGE;
+  const [bookingRequest, setbookingRequest] = useState(props.bookingRequest);
+  const pageCount = Math.ceil(bookingRequest.length / PER_PAGE);
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [currentPageData, setCurrentPageData] = useState(
+    bookingRequest.slice(offset, offset + PER_PAGE)
+  );
 
-    const PER_PAGE = 10;
-    const offset = currentPage * PER_PAGE;
-    const currentPageData = bookingRequest.slice(offset, offset + PER_PAGE);
-    const pageCount = Math.ceil(bookingRequest.length / PER_PAGE);
-  
-    function openModal() {
-      // fetchData();
-      setIsOpen(true);
+  function openModal() {
+    // fetchData();
+    setIsOpen(true);
+  }
+
+  function handlePageClick({ selected: selectedPage }) {
+    setCurrentPage(selectedPage);
+  }
+
+  const handleRowCLick = (id) => {};
+
+  useEffect(() => {
+    let finalData = [...currentPageData];
+    if (props.ccaFilter.length != 0) {
+      finalData.filter((req) => props.ccaFilter.includes(req.cca));
+    }
+    if (props.venueFilter.length != 0) {
+      finalData.filter((req) => props.venueFilter.includes(req.venue));
+    }
+    if (props.dateFilter.length != 0) {
+      finalData.filter((req) => props.dateFilter.includes(req.date));
     }
 
-    function handlePageClick({ selected: selectedPage }) {
-      setCurrentPage(selectedPage);
-    }
+    setCurrentPageData(finalData);
+  }, [props.ccaFilter, props.venueFilter, props.dateFilter]);
 
-    const handleRowCLick= (id) => {
-    } 
-    
-    return (
-        <div style={{width: '100%'}}>
-        <table>
-            <thead>
-                <th><CheckBox></CheckBox></th>
-                <th className='BookingRequestPageVenueHeader'>Venue</th>
-                <th className='BookingRequestPageCCAHeader'>CCA</th>
-                <th className='BookingRequestPageTimeBookedHeader'>Time Booked</th>
-                <th className='BookingRequestPagePurposeHeader'>Purpose</th>
-                <th></th>
-            </thead>
-                {currentPageData.map((req) => 
-                <tbody>
-                    <tr onClick={() => handleRowCLick(req.id)}>
-                    <td><CheckBox></CheckBox></td>
-                    <td>{req.venue.name}</td> 
-                    <td>{req.cca}</td>
-                    <td style={{display: 'inline-flex', justifyContent:"space-around"}}>{req.timingSlots.map((slot) =>
-                        <div style={{padding:'4px',marginRight:'4px',background: "#ECDBBA", borderRadius: '1px', fontWeight:'500'}}>
-                            {slot}
-                        </div>
-                    )}</td>
-                    <td>{req.notes}</td>
-                    <td> 
-                    <SpecificReqModal req={req} bookingRequests={bookingRequest}/>
+  return (
+    <div style={{ width: "100%" }}>
+      <table>
+        <thead>
+          <th>
+            <CheckBox></CheckBox>
+          </th>
+          <th className="BookingRequestPageVenueHeader">Venue</th>
+          <th className="BookingRequestPageCCAHeader">CCA</th>
+          <th className="BookingRequestPageTimeBookedHeader">Time Booked</th>
+          <th className="BookingRequestPagePurposeHeader">Purpose</th>
+          <th></th>
+        </thead>
+        {currentPageData.map((req) => (
+          <tbody>
+            <tr onClick={() => handleRowCLick(req.id)}>
+              <td>
+                <CheckBox></CheckBox>
+              </td>
+              <td>{req.venue.name}</td>
+              <td>{req.cca}</td>
+              <td
+                style={{
+                  display: "inline-flex",
+                  justifyContent: "space-around",
+                }}
+              >
+                {req.timingSlots.map((slot) => (
+                  <div
+                    style={{
+                      padding: "4px",
+                      marginRight: "4px",
+                      background: "#ECDBBA",
+                      borderRadius: "1px",
+                      fontWeight: "500",
+                    }}
+                  >
+                    {slot}
+                  </div>
+                ))}
+              </td>
+              <td>{req.notes}</td>
+              <td>
+                <SpecificReqModal req={req} bookingRequests={bookingRequest} />
+              </td>
+            </tr>
+          </tbody>
+        ))}
+      </table>
 
-                        {/* <Link className="adminBookingRequestApprove" onClick={() => {openModal()}}>
-                            Approve 
-                        </Link>
-                        <Link className="adminBookingRequestReject">
-                            Reject
-                        </Link> */}
-                    </td>
-                    </tr>
-                </tbody>
-                )}
-        </table>
-        
-        <ReactPaginate
-          previousLabel={"←"}
-          nextLabel={"→"}
-          pageCount={pageCount}
-          onPageChange={handlePageClick}
-          containerClassName={"pagination"}
-          previousLinkClassName={"pagination__link"}
-          nextLinkClassName={"pagination__link"}
-          disabledClassName={"pagination__link--disabled"}
-          activeClassName={"pagination__link--active"}
-        />
+      <ReactPaginate
+        previousLabel={"←"}
+        nextLabel={"→"}
+        pageCount={pageCount}
+        onPageChange={handlePageClick}
+        containerClassName={"pagination"}
+        previousLinkClassName={"pagination__link"}
+        nextLinkClassName={"pagination__link"}
+        disabledClassName={"pagination__link--disabled"}
+        activeClassName={"pagination__link--active"}
+      />
     </div>
-    )
-}
+  );
+};
 
-export default PendingRequest
+export default PendingRequest;
